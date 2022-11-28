1c39b1678368b0d182032bfa6936af2a42dd9e29
imskanand <the.anand3099@gmail.com>
2022-11-28 20:01:09 +0530

    Type casting in Java

--- src/main/java/org/imskanand/Basic/SectionTwo/TypeCasting.java
+++ src/main/java/org/imskanand/Basic/SectionTwo/TypeCasting.java
@@ -3,5 +3,16 @@ package org.imskanand.Basic.SectionTwo;
 public class TypeCasting {
     public static void main(String[] args) {
 
+        // float to int conversions
+        float myAge = 21.9f;
+        int myAgeInt = (int)myAge;
+        System.out.println("My Age : "+ myAge);
+        System.out.println("My Age : "+ myAgeInt);
+
+        // int to float conversions
+        int mySalary = 3244;
+        float mySalaryFloat = (float)mySalary;
+        System.out.println("My Salary : "+ mySalary);
+        System.out.println("My Salary : "+ mySalaryFloat);
     }
 }

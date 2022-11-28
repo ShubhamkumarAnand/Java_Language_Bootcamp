eff99af01a427f50c7a81bb1ca78756307b7481b
imskanand <the.anand3099@gmail.com>
2022-11-28 18:51:43 +0530

    Completed the first project

---
+++ src/main/java/org/imskanand/Basic/NameInitials.java
@@ -0,0 +1,14 @@
+package org.imskanand.Basic;
+
+public class NameInitials {
+    public static void main(String[] args) {
+        String str = ">>:   SSS     A  \n" +
+                ">>:  S   S   A A \n" +
+                ">>:  S      A   A\n" +
+                ">>:   SSS   AAAAA\n" +
+                ">>:      S  A   A\n" +
+                ">>:  S   S  A   A\n" +
+                ">>:   SSS   A   A";
+        System.out.println(str);
+    }
+}

b68f3c56e52fdc9ba160a936013918ec5eae1623
imskanand <the.anand3099@gmail.com>
2022-12-05 10:53:14 +0530

    Guess the Number using the While Loop!

---
+++ src/main/java/org/imskanand/Basic/SectionFive/GuessTheNumber.java
@@ -0,0 +1,21 @@
+package org.imskanand.Basic.SectionFive;
+
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static int randomNumber() {
+        return (int)(Math.random() * 6);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Guess the number between 1-5!");
+        int number = randomNumber();
+        int guessedNumber = scan.nextInt();
+        while(!(guessedNumber==number)) {
+            System.out.print("Guess Again : ");
+            guessedNumber = scan.nextInt();
+        }
+        System.out.println("You are Right!");
+    }
+}
